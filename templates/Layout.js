49d7ae940d9d1fa75315d047a011e1791a97e621
Mohammad Mahdi <mo.mahdi.ka@gmail.com>
2024-12-14 10:06:34 +0330

    layout complated

--- templates/Layout.js
+++ templates/Layout.js
@@ -1,6 +1,9 @@
 import Link from "next/link";
 import styles from "./Layout.module.css";
 import { RiShareBoxFill } from "react-icons/ri";
+import { FaListCheck } from "react-icons/fa6";
+import { GrAddCircle } from "react-icons/gr";
+import { CgProfile } from "react-icons/cg";
 function Layout({ children }) {
   return (
     <>
@@ -15,8 +18,30 @@ function Layout({ children }) {
           </div>
         </nav>
       </header>
-      <aside></aside>
-      <section>{children}</section>
+      <aside className={styles.sidebar}>
+        <h4>Welcome 👋</h4>
+        <ul>
+          <li>
+            <Link href="/">
+              <FaListCheck />
+              Todos
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              <GrAddCircle />
+              Add Todo
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              <CgProfile />
+              Profile
+            </Link>
+          </li>
+        </ul>
+      </aside>
+      <section className={styles.section}>{children}</section>
       <footer className={styles.footer}>
         <p>
           Developer :
