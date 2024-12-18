43e07eb92fae7e5a1aff0af015baf7c0f2ed7a1b
Mohammad Mahdi <mo.mahdi.ka@gmail.com>
2024-12-18 14:29:35 +0330

    authentication index page  in front

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
-import React from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 function index() {
-  return <div></div>;
+  const { status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status == "unauthenticated") router.push("/signin");
+  }, [status]);
+
+  if (status == "loading") {
+    return <div>loading ...</div>;
+  }
+  if (status == "authenticated") {
+    return <div>welcome</div>;
+  }
+  return <></>;
 }
 
 export default index;
